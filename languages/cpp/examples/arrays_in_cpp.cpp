67245ec40502bd1e3fef9139c34cd73a6c65e536
N3M3S1Spy <161128864+N3M3S1Spy@users.noreply.github.com>
2024-11-07 05:35:54 +0100

    Create arrays_in_cpp.cpp

---
+++ languages/cpp/examples/arrays_in_cpp.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    // Array-Ausgabe
+    for (int i = 0; i < 5; ++i) {
+        std::cout << "Element " << i << ": " << arr[i] << std::endl;
+    }
+
+    return 0;
+}
