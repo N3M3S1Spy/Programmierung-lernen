7c4053aefc336577478e02e384b615bd7379eb06
N3M3S1Spy <161128864+N3M3S1Spy@users.noreply.github.com>
2024-11-07 05:34:55 +0100

    Create kontrollstrukturen_in_cpp.cpp

---
+++ languages/cpp/examples/kontrollstrukturen_in_cpp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    int number = 10;
+
+    // if-else Beispiel
+    if (number > 5) {
+        std::cout << "Zahl ist größer als 5." << std::endl;
+    } else {
+        std::cout << "Zahl ist kleiner oder gleich 5." << std::endl;
+    }
+
+    // for-Schleife Beispiel
+    for (int i = 0; i < 5; ++i) {
+        std::cout << "i ist: " << i << std::endl;
+    }
+
+    return 0;
+}
