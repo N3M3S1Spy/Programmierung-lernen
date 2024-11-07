52e670ee8d836c121a5a187ffcb6d175346e6377
N3M3S1Spy <161128864+N3M3S1Spy@users.noreply.github.com>
2024-11-07 05:34:29 +0100

    Create operatoren_in_cpp.cpp

---
+++ languages/cpp/examples/operatoren_in_cpp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int a = 10, b = 20;
+
+    std::cout << "Addition: " << (a + b) << std::endl;
+    std::cout << "Subtraktion: " << (a - b) << std::endl;
+    std::cout << "Multiplikation: " << (a * b) << std::endl;
+    std::cout << "Division: " << (b / a) << std::endl;
+    std::cout << "Modulo: " << (b % a) << std::endl;
+
+    return 0;
+}
