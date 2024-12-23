4195a862de9ab6348f374730efde539d9a4a5c96
N3M3S1Spy <161128864+N3M3S1Spy@users.noreply.github.com>
2024-12-23 13:38:33 +0100

    Create syntax.cpp

---
+++ languages/cpp/tutorials/syntax.cpp
@@ -0,0 +1,11 @@
+# C++ Syntax
+Zerlegen wir den folgenden Code, um ihn besser zu verstehen:
+```c++
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout << "Hello World!";
+  return 0;
+} 
+```
